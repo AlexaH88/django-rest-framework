95b71c4faf0d0027dfdba2a1c6062855c69a6b40
Alexa Hendry <alexahendry88@gmail.com>
2023-03-17 16:28:22 +0000

    Added JSON Web Tokens

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -27,6 +27,30 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        # uses sessions in development
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        # uses tokens in production
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+
+JWT_AUTH_SECURE = True
+
+# access token
+JWT_AUTH_COOKIE = 'my-app-auth'
+
+# refresh token
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZER = {
+    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -53,6 +77,13 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 
     'profiles',
     'posts',
@@ -60,6 +91,8 @@ INSTALLED_APPS = [
     'likes',
     'followers',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
